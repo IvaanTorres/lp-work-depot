0336c9a05ce5c74c50f9403a5edabdcf520bb686
IvaanTorres <ivantorresgarcia.02@gmail.com>
2022-12-13 15:44:51 +0100

    feat(architecture): add uploads to projects

--- database/migrations/2022_12_13_144031_create_uploads_table.php
+++ database/migrations/2022_12_13_144031_create_uploads_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('uploads', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
-            $table->string('upload'); /* Change to blob: It can be pdf, image, etc */
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('uploads');
     }
 };
