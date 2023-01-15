a30a6d0a9c1f31b50f6c9d62b14e3b8183e94f25
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-01-15 17:56:22 +0100

    perf(routes): group routes

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 /* ----------------------------- User Controller ---------------------------- */
 Route::controller(UserController::class)->group(function () {
   /* Auth */
-  Route::prefix('auth')->group(function () {
+  Route::prefix('/auth')->group(function () {
     Route::get('/register', 'show_register')->name('register_page');
     Route::post('/register', 'register')->name('register');
     Route::get('/login', 'show_login')->name('login_page');
@@ -30,52 +30,52 @@ Route::controller(UserController::class)->group(function () {
     Route::post('/logout', 'logout')->name('logout');
   });
 });
-/* -------------------------------- Dashboard ------------------------------- */
-// Route::view('/dashboard', 'dashboard.index')
-//   ->middleware(['roles:student', 'auth'])
-//   ->name('dashboard_page');
 
 /* ---------------------------------- Course --------------------------------- */
 Route::controller(CourseController::class)->group(function () {
-  Route::get('/courses', 'index')->name('courses_list_page');
-  Route::get('/courses/create', 'create')->name('course_creation_page');
-  Route::get('/courses/{course_id}/edit', 'edit')->name('course_modification_page');
-  Route::get('/courses/{course_id}', 'show')->name('course_details_page');
+  Route::prefix('/courses')->group(function () {
+    Route::get('/', 'index')->name('courses_list_page');
+    Route::get('/create', 'create')->name('course_creation_page');
+    Route::get('/{course_id}/edit', 'edit')->name('course_modification_page');
+    Route::get('/{course_id}', 'show')->name('course_details_page');
 
-  Route::post('/courses', 'store')->name('course_creation');
-  Route::put('/courses/{course_id}', 'update')->name('course_modification');
-  Route::delete('/courses/{course_id}', 'destroy')->name('course_deletion');
+    Route::post('/', 'store')->name('course_creation');
+    Route::put('/{course_id}', 'update')->name('course_modification');
+    Route::delete('/{course_id}', 'destroy')->name('course_deletion');
 
-  Route::get('/courses/{course_id}/users', 'getUsers')->name('course_users_page');
-  Route::post('/courses/{course_id}/users', 'linkUser')->name('course_link_users');
-  Route::put('/courses/{course_id}/users/{user_id}', 'unlinkUser')->name('course_unlink_users');
+    Route::get('/{course_id}/users', 'getUsers')->name('course_users_page');
+    Route::post('/{course_id}/users', 'linkUser')->name('course_link_users');
+    Route::put('/{course_id}/users/{user_id}', 'unlinkUser')->name('course_unlink_users');
+  });
 });
 
 /* --------------------------------- Lessons -------------------------------- */
 Route::controller(LessonController::class)->group(function () {
-  /* Dont't think it is necessary */
-  // Route::get('/courses/{course_id}/lessons/{lesson_id}', 'show')->name('lesson_details_page');
-  Route::get('/courses/{course_id}/lessons/create', 'create')->name('lesson_creation_page');
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/edit', 'edit')->name('lesson_modification_page');
+  Route::prefix('/courses/{course_id}/lessons')->group(function () {
+    Route::get('/{lesson_id}/edit', 'edit')->name('lesson_modification_page');
+    Route::get('/create', 'create')->name('lesson_creation_page');
 
-  Route::post('/courses/{course_id}/lessons', 'store')->name('lesson_creation');
-  Route::put('/courses/{course_id}/lessons/{lesson_id}', 'update')->name('lesson_modification');
-  Route::delete('/courses/{course_id}/lessons/{lesson_id}', 'destroy')->name('lesson_deletion');
+    Route::post('/', 'store')->name('lesson_creation');
+    Route::put('/{lesson_id}', 'update')->name('lesson_modification');
+    Route::delete('/{lesson_id}', 'destroy')->name('lesson_deletion');
+  });
 });
 
 /* -------------------------------- Projects -------------------------------- */
 Route::controller(ProjectController::class)->group(function () {
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/users', 'getUsers')->name('project_users_page');
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/users/{user_id}', 'getUserDetails')->name('project_user_details_page');
-  Route::put('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/users/{user_id}', 'evaluate')->name('project_evaluate');
+  Route::prefix('/courses/{course_id}/lessons/{lesson_id}/projects')->group(function () {
+    Route::get('/{project_id}/users', 'getUsers')->name('project_users_page');
+    Route::get('/{project_id}/users/{user_id}', 'getUserDetails')->name('project_user_details_page');
+    Route::put('/{project_id}/users/{user_id}', 'evaluate')->name('project_evaluate');
 
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/create', 'create')->name('project_creation_page');
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/edit', 'edit')->name('project_modification_page');
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}', 'show')->name('project_details_page'); /* List user uploads for teachers */
+    Route::get('/create', 'create')->name('project_creation_page');
+    Route::get('/{project_id}/edit', 'edit')->name('project_modification_page');
+    Route::get('/{project_id}', 'show')->name('project_details_page'); /* List user uploads for teachers */
 
-  Route::post('/courses/{course_id}/lessons/{lesson_id}/projects', 'store')->name('project_creation');
-  Route::put('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}', 'update')->name('project_modification');
-  Route::delete('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}', 'destroy')->name('project_deletion');
+    Route::post('/', 'store')->name('project_creation');
+    Route::put('/{project_id}', 'update')->name('project_modification');
+    Route::delete('/{project_id}', 'destroy')->name('project_deletion');
+  });
 });
 
 /* ---------------------------------- Users --------------------------------- */
@@ -92,18 +92,17 @@ Route::controller(UserController::class)->group(function () {
 
 /* ------------------------------ User Uploads ------------------------------ */
 Route::controller(UploadController::class)->group(function () {
-  // Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/{uploads_id}', 'index')->name('upload_details_page');
-  Route::post('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/{file_id}/download', 'download_file')->name('upload_file_download');
-  Route::delete('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/{document_id}', 'destroy')->name('upload_deletion');
+  Route::prefix('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads')->group(function () {
+    // Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/{uploads_id}', 'index')->name('upload_details_page');
+    Route::post('/{file_id}/download', 'download_file')->name('upload_file_download');
+    Route::delete('/{document_id}', 'destroy')->name('upload_deletion');
 
-  /* --------------------------------- General -------------------------------- */
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/create', 'index')->name('upload_creation_page');
-  Route::post('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads', 'store')->name('upload_creation');
+    Route::get('/create', 'index')->name('upload_creation_page');
+    Route::post('/', 'store')->name('upload_creation');
 
-  Route::get('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads/edit', 'index')->name('upload_modification_page');
-  Route::put('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads', 'index')->name('upload_modification');
-
-  Route::delete('/courses/{course_id}/lessons/{lesson_id}/projects/{project_id}/uploads', 'index')->name('upload_modification_page');
+    Route::get('/edit', 'index')->name('upload_modification_page');
+    Route::put('/', 'index')->name('upload_modification');
+  });
 });
 
 /* -------------------------------- 404 Error ------------------------------- */
