c9f9ca711f425cf361aabacc437ca1c5b6c65d95
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-01-29 12:55:24 +0100

    feat(404): rework the 404 error page style

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -3,5 +3,6 @@
 @section('title', 'Error 404')
 
 @section('content')
-  <p>Error 404</p>
+  <h3 class="font-semibold text-4xl mb-2">Oops!</h3>
+  <p class="text-lg">We can't seem to find the page you're looking for.</p>
 @endsection
